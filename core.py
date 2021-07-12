4dd5fdde4fb92daaa47f02143005853b72828346
ngc660 <87309107+ngc660@users.noreply.github.com>
2021-07-12 14:18:58 +0300

    Add files via upload

--- core.py
+++ core.py
@@ -7,10 +7,18 @@ activiti = True
 disp = pygame.display.set_mode((800,600))
 pygame.display.set_caption("2048")
 frame = 50
+
+def drawing():
+    disp.fill((0,60,190))
+    pass
+def updating():
+    pass
 while activiti:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             activiti = False
+    updating()
+    drawing()
     pygame.display.flip()
     pygame.time.delay(frame)
     pass
